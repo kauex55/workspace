8381339bb97e57dd0cc12a4d0c9aade3510c53f1
kauex55 <kauelima2477@gmail.com>
2023-07-25 12:56:05 -0300

    Atividade Menu

---
+++ menu.py
@@ -0,0 +1,51 @@
+import os
+
+#men
+
+Np=1
+Nd=1
+Md=1
+s=1
+
+while mi == 1:
+     
+os.system("cls")
+
+print ("Bem vindo ao menu")
+print("[1] Número primo")
+print("[2] Número decrescente")
+print("[3] Mediana")
+print("[4] Sair")
+x = int(input(""))
+
+#//////////////////////////////////////////////////////////////////////
+
+in np1 == 1:
+    np = 1
+np1 = 0
+
+while np == 1
+    os.system("cls")
+    print ("Bem vindo ao menu")
+    print("[1] Número primo")
+    print("[2] Número decrescente")
+    print("[3] Mediana")
+    print("[4] Sair")
+    x = int(input(""))
+
+    trava = 0
+
+    if x == 1:
+        if trava == 0
+            print("Iniciou a lógica dos números primos")
+            trava = 1
+        else:
+             print("Ja foi executada a lógica do programa")
+    elif x == 2:
+            if trava == 0
+            print("Refez a lógica dos numeros primos")
+    elif x == 3:
+            print("voltar")
+    else:
+          print("Oção invalida")
+          os.system("pause")
